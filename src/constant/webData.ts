2c21b4cb95de36c1c2445c77f516c43dd188f7fd
Anas Ali <anasalimgmt@gmail.com>
2024-08-25 21:33:36 +0500

    Update
    Building my LandingPage on my React JS Project

---
+++ src/constant/webData.ts
@@ -0,0 +1,60 @@
+const landingPageData = {
+    header: {
+        title:"welcome to ZipWallet",
+        subtitle: "Manage your Finances with ease and confidence",
+        logo:"",
+    },
+    hero: {
+        title: "Take Control of Your Finance Today",
+        description:"Track expenses, set budget, and achieve your financial goal with financeGuru.",
+        buttonText: "Get Started Now",
+        image:"https://th.bing.com/th/id/R.a704076c43301c7884d794de1102776a?rik=PjWQgA3ztCrg1A&pid=ImgRaw&r=0",
+    },
+    features: [
+        {
+            id: 1,
+            icon:"📊",
+            title:"Expense Tracking",
+            description:
+            "Easily track your daily expenses and see where your money goes",
+        },
+        {
+            id: 2,
+            icon:"💰",
+            title:"Budgeting",
+            description:
+            "Set monthly budgets and stick to them to save more.",
+        },
+        {
+            id: 3,
+            icon:"📈",
+            title:"Financial Goals",
+            description:
+            "Set and acheive your financial goals with personalized tips and insights.",
+        },
+    ],
+    testimonials: [
+        {
+            id: 1,
+            name: "Alice Johnson",
+            feedback:
+            "ZipWallet has completely transformed the ways I manage my money, Highly Recommended!",
+            avatar: "https://example.com/alice.jpg",
+        },
+        {
+            id: 2,
+            name: "Bob Lee",
+            feedback:
+            "The budgeting feature is fantastic! I've saved so much more since using this app.",
+            avatar: "https://example.com/bob.jpg",
+        },
+    ],
+    callToAction: {
+        title: "Join Thousand of Satisfied Users",
+        description:
+        "Sign up today and take the first step towards financial freedom.",
+        buttonText:"Sign Up Now.",
+    },
+};
+
+export default landingPageData;
